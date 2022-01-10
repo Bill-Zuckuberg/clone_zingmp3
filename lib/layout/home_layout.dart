8e215e6f61fbd83805df962a3b3af6cb09cd48b9
Bill-Zuckuberg <thanh821329@gmail.com>
2022-01-11 04:16:01 +0700

    bat dau lam phan layout individual

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -91,7 +91,57 @@ class _HomeLayoutState extends State<HomeLayout>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(children: _kTabPages, controller: _tabcontroller),
+      body: Column(
+        children: [
+          // Phần TabBarView
+          Expanded(
+              child:
+                  TabBarView(children: _kTabPages, controller: _tabcontroller)),
+          // Phần hiển thị nhạc đang nghe
+          Container(
+            height: 55,
+            color: Colors.red.withOpacity(0.2),
+            child: Column(
+              children: [
+                Container(
+                  height: 5,
+                  color: colors.AppColors.AppColor,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      child: CircleAvatar(
+                        child: Icon(
+                          Icons.access_time,
+                          color: colors.AppColors.AppColor,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text("Slider Text"),
+                                Text("Tên ca sĩ")
+                              ],
+                            ))),
+                    const Icon(Icons.favorite_border),
+                    const Icon(Icons.play_arrow),
+                    const Icon(Icons.arrow_right_outlined),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      // Phần BottomBar
       bottomNavigationBar: Material(
         child: TabBar(
           tabs: _kTabs,
--- lib/layout/individual_layout.dart
+++ lib/layout/individual_layout.dart
@@ -10,9 +10,38 @@ class IndividualLayout extends StatefulWidget {
 class _IndividualLayoutState extends State<IndividualLayout> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(child: CircularProgressIndicator()),
+    return Container(
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          // Phần Search
+          Row(
+            children: [
+              Container(
+                child: const CircleAvatar(
+                  child: Icon(Icons.access_alarm),
+                ),
+              ),
+              Expanded(child: Container()),
+              const Icon(Icons.settings)
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text(
+            "Thư Viện",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          // Phần .....
+        ],
       ),
     );
   }
